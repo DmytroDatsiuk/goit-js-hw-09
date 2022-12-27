a0600c9699f6997bac5ca75c5d766d73a8f4d8dc
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2022-12-27 10:36:02 +0200

    03fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,16 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
-const input = document.querySelector('input#datetime-picker');
-const startButton = document.querySelector('[data-start]');
-
-startButton.addEventListener('click', onStartButtonClick);
+const refs = {
+  input: document.querySelector('input#datetime-picker'),
+  startButton: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
 const options = {
   enableTime: true,
@@ -12,24 +18,39 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(options.defaultDate.getTime())
-    if (Number(selectedDates[0]) <= options.defaultDate.getTime()) {
-      window.alert('Please choose a date in the future');
+    if (Number(selectedDates[0]) <= Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
       return;
     }
 
-    startButton.classList.remove('disable-timer');
-    startButton.classList.add('is-active-timer');
+    refs.startButton.classList.remove('disable-timer');
+    refs.startButton.classList.add('is-active-timer');
 
-    console.log(Number(selectedDates[0]));
+
+    refs.startButton.addEventListener('click', () => {
+      setInterval(() => {
+        const timerTime = Number(selectedDates[0]) - Date.now();
+
+        refs.days.textContent = String(convertMs(timerTime).days).padStart(
+          2,
+          0
+        );
+        refs.hours.textContent = String(convertMs(timerTime).hours).padStart(
+          2,
+          0
+        );
+        refs.minutes.textContent = String(
+          convertMs(timerTime).minutes
+        ).padStart(2, 0);
+        refs.seconds.textContent = String(
+          convertMs(timerTime).seconds
+        ).padStart(2, 0);
+      }, 1000);
+    });
   },
 };
 
-flatpickr(input, options);
-
-function onStartButtonClick(e) {
-    
-}
+flatpickr(refs.input, options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -50,6 +71,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return String(convertMs(timerTime).hours).padStart(2, 0)
+}
