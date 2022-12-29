66efc9139f9d447c3c03c6b310d3aaadf1a3fa3d
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2022-12-29 13:34:25 +0200

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+// import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', promisesGenerator);
+
+function promisesGenerator(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.currentTarget.elements;
+
+  createPromise(1, Number(delay.value))
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  for (let i = 2; i <= Number(amount.value); i++) {
+    createPromise(i, Number(step.value) * (i - 1)+ Number(delay.value))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        res({ position, delay });
+      } else {
+        // Reject
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
